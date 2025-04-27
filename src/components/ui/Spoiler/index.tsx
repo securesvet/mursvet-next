14436724cff2241ca32b36fc64383ed11a25bffe
Sviatoslav Murzin <mursvyats@gmail.com>
2025-04-28 04:52:45 +0500

    feat: made spoiler tag inline

--- src/components/ui/Spoiler/index.tsx
+++ src/components/ui/Spoiler/index.tsx
@@ -9,16 +9,14 @@ export default function Spoiler({ children }: { children: React.ReactNode }) {
   const [revealed, setRevealed] = useState(false);
 
   return (
-    <div onClick={() => setRevealed(true)}>
-      <span
-        className={cn(
-          "transition-all ",
-          !revealed && "spoiler text-transparent",
-          revealed && "text-opacity-100",
-        )}
-      >
-        {children}
-      </span>
-    </div>
+    <span
+      onClick={() => setRevealed(true)}
+      className={cn(
+        "transition-all",
+        !revealed && "cursor-pointer spoiler text-transparent",
+      )}
+    >
+      {children}
+    </span>
   );
 }
