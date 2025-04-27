2c809560fb1ab7a3f21fe64fbdf89e961c24b80a
Sviatoslav Murzin <mursvyats@gmail.com>
2025-04-27 14:29:19 +0500

    feat: added robots.txt and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://mursvet.ru',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://mursvet.ru/resume',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://mursvet.ru/writeups',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://mursvet.ru/projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
